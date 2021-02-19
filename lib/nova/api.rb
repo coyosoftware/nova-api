32bd5057f80990f5dce9d923313c6e918f41955d
Gustavo Henrique Vicensotti Berdugo <gberdugo@gmail.com>
2021-02-19 16:19:09 -0300

    Commit inicial

---
+++ lib/nova/api.rb
@@ -0,0 +1,8 @@
+require "nova/api/version"
+
+module Nova
+  module API
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/nova/api/version.rb
@@ -0,0 +1,5 @@
+module Nova
+  module API
+    VERSION = "0.1.0"
+  end
+end
