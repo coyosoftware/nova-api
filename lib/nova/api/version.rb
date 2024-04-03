988373611288bde6957434902bdd0ae9f64a23ce
Gustavo Berdugo <gustavo@coyo.com.br>
2024-04-03 11:18:11 -0300

    Added debug flag

--- lib/nova/api/version.rb
+++ lib/nova/api/version.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 module Nova
   module API
-    VERSION = "1.4.2"
+    VERSION = '1.4.3'
   end
 end
--- lib/nova/configuration.rb
+++ lib/nova/configuration.rb
@@ -1,12 +1,16 @@
 module Nova
   module API
     class Configuration
-      attr_accessor :api_key, :subdomain, :use_staging
+      attr_accessor :api_key, :subdomain, :use_staging, :debug
       attr_reader :host
       attr_reader :scheme
 
       def use_staging?
         [true, 'true'].include?(use_staging)
+      end
+
+      def debug?
+        [true, 'true'].include?(debug)
       end
     end
   end
